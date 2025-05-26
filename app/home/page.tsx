58081edd11c0fd9521e7b859c86d60f42b7a75b7
Rosier Anderson <rosieranderson@Rosiers-Mac-mini.local>
2025-05-26 16:37:58 -0400

    styling nav-links

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -2,38 +2,46 @@
 import PreHeader from "../ui/home/pre-header";
 import Image from "next/image";
 import Logo from "../../public/logo.svg";
-// Importing the logo image
+// Importing the hero image
 import HeroImage from "../../public/hero-header.png";
-import MenuLinks from "../ui/home/header-nav-links";
+import NavLinks from "../ui/home/nav-links";
+import { FaPhoneVolume } from "react-icons/fa";
 export default function HomePage({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <div className="w-screen h-screen bg-neutral-100 ">
       <main className="  w-[96%] h-[96%] flex flex-col  mr-auto ml-auto  ">
-       <PreHeader />
-  <div className="relative w-[96%] h-[96%] mr-auto ml-auto">
-      <Image
-        src={HeroImage}
-        alt="Hero background image"
-        fill
-        className="object-cover rounded-2xl"
-        priority
-      />
-      <header className="absolute inset-x-0 top-0   text-white">
-   <div className="flex items-center gap-4 ">
-    <Image
-      src={Logo}
-      alt="Logo"
-      />
-<div>
-    <MenuLinks />
-   </div>
-   </div>
-   
-      </header>
-    </div>
-
+        <PreHeader />
+        <div className="relative w-[96%] h-[96%] mr-auto ml-auto">
+          <div>
+             <Image
+            src={HeroImage}
+            alt="Hero background image"
+            fill
+            className="object-cover rounded-2xl"
+            priority
+          />
+          </div>
+         
+          <header className="absolute inset-x-0 top-0 ml-0  text-white w-full h-16 flex items-center justify-between p-4 m-8 ">
+            <div className="flex items-center gap-10  ">
+              <div className=" ">
+                <Image src={Logo} alt="Logo" />
+              </div>
+              
+            
+              <NavLinks />
+            
+            </div>
+            <div className=" flex items-center ">
+              <div>
+                <FaPhoneVolume/>
+                <span>+1 (514)-25566-22</span>
+              </div>
+            </div>
+          </header>
+        </div>
       </main>
     </div>
   );
--- app/ui/home/nav-links.tsx
+++ app/ui/home/nav-links.tsx
@@ -7,11 +7,11 @@ export default function MenuLinks() {
     { name: "Contact", href: "/contact" },
   ];
   return (
-    <div className="bg-zinc  rounded-2xl w-full h-full">
+    <div className="bg-zinc-100/60  rounded-4xl h-15 flex items-center justify-between px-4">
       <nav className="">
-        <ul className="flex items-center gap-4 text-lg font-bold text-white ">
+        <ul className="flex items-center gap-10 text-lg font-bold text-white ">
           {navLinks.map((link) => (
-            <li key={link.name} className="">
+            <li key={link.name} className="cursor-pointer ">
               {link.name}
             </li>
           ))}

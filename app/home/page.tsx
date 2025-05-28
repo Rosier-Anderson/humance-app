ab4038c2b5303678b03879a01cffbc43ebd851a1
Anderson <97542626+Rosier-Anderson@users.noreply.github.com>
2025-05-28 09:53:09 +0000

    Refactor HomePage component: Layout, Responsiveness, and Styling Updates

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -9,46 +9,48 @@ export default function HomePage({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
-    <div className="w-screen h-screen bg-neutral-100">
-      <main className="w-[96%] h-[96%] flex flex-col mx-auto">
-        <PreHeader />
-        <div className="relative size-full mx-auto">
-          <Image
-            src={HeroImage}
-            alt="Hero background image"
-            fill
-            className="object-cover rounded-2xl"
-            priority
-          />
-          <Header />
-          <HeroText />
+    <main className=" flex flex-col mx-auto  bg-neutral-100">
+      <PreHeader />
+      <div className="relative size-full mx-auto ">
+        <Image
+          width={1920}
+          height={1080}
+          src={HeroImage}
+          alt="Hero background image"
+          className="object-cover rounded-2xl size-full"
+          priority
+        />
+
+        <Header />
+        <HeroText />
+      </div>
+      {/* sections  */}
+      <section>
+        <div className=" ">
+          <span></span> <p className="whitespace-nowrap">ABOUT US </p>
         </div>
-        {/* sections  */}
-        <section>
-          <h1>
-            Empowering organizations to thrive through cutting-edge HR
-            thecnology{" "}
-          </h1>
-          <p>
-            Where the future of energy is not just a concept it's our mission.
-            Born from the vision of <br />
-            pionning sustainable world, we are more tham just a company. We
-            provide you the best <br />
-            service quality with the best rate all the time no matter.
-          </p>
-          <div>
-            <article>
-              <h1>Plannind and Analytics </h1>
-              {/* the icon goes here  */}
-              <hr />
-              <p>
-                Metus torquent platea aenean orci ridiculus potenci feugiat
-                ultricies.
-              </p>
-            </article>
-          </div>
-        </section>
-      </main>
-    </div>
+        <h1>
+          Empowering organizations to thrive through cutting-edge HR thecnology{" "}
+        </h1>
+        <p>
+          Where the future of energy is not just a concept it's our mission.
+          Born from the vision of <br />
+          pionning sustainable world, we are more tham just a company. We
+          provide you the best <br />
+          service quality with the best rate all the time no matter.
+        </p>
+        <div>
+          <article>
+            <h1>Plannind and Analytics </h1>
+            // the icon goes here
+            <hr />
+            <p>
+              Metus torquent platea aenean orci ridiculus potenci feugiat
+              ultricies.
+            </p>
+          </article>
+        </div>
+      </section>
+    </main>
   );
 }
